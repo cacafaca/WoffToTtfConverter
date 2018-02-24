1d6aa359d7fae227a8869e8e17e6f4e6763ea7ed
Nemanja Simović <nemanja.simovic@gmail.com>
2018-02-24 18:01:22 +0100

    Reorganize projects...

--- Woff2Ttf/ProCode.Woff2Ttf/Converter.cs
+++ Woff2Ttf/ProCode.Woff2Ttf/Converter.cs
@@ -25,47 +25,6 @@ namespace ProCode.Woff2Ttf
             return null;
         }
 
-        /// <summary>
-        /// Reads UIntBase128 Data Type as written in https://www.w3.org/TR/WOFF2/.
-        /// </summary>
-        /// <param name="encodedStream"></param>
-        /// <param name="result"></param>
-        public static bool ReadUIntBase128(Stream encodedStream, out UInt32 result)
-        {
-            Stream decodedStream = new MemoryStream();
-            result = UInt32.MinValue;
-
-            if (encodedStream.CanRead)
-            {
-                UInt32 accum = 0;
-                for (int i = 0; i < 5; i++)
-                {
-                    var dataByte = encodedStream.ReadByte();
-
-                    // No leading 0's.  0x80 = 1000 0000
-                    if (i == 0 && dataByte == 0x80)
-                        return false;
-
-                    // If any of top 7 bits are set then << 7 would overflow. 0xfe000000 = 1111 1110 0000 0000 0000 0000 0000 0000.
-                    if ((accum & 0xfe000000) > 1)
-                        return false;
-
-                    accum = (accum << 7) | ((UInt32)dataByte & 0x7f); // 0x7f = 0111 1111
-
-                    // Spin until most significant bit of data byte is false.
-                    if((dataByte & 0x80) != 0)
-                    {
-                        result = accum;
-                        return true;
-                    }
-                }
-                // UIntBase128 sequence exceeds 5 bytes.
-                return false;
-            }
-            else
-                throw new Exception("Can't read stream.");
-        }
-
         #endregion
     }
 }
--- Woff2Ttf/ProCode.Woff2TtfTests/ConverterTests.cs
+++ Woff2Ttf/ProCode.Woff2TtfTests/ConverterTests.cs
@@ -18,15 +18,5 @@ namespace ProCode.Woff2Ttf.Tests
             Assert.Fail();
         }
 
-        [TestMethod()]
-        public void ReadUIntBase128Test()
-        {
-            Stream stream = new MemoryStream(new byte[6] { 0x1c, 0x1a, 0x81, 0x2e, 0x1b, 0xa1 });
-            UInt32 expected = 0x070d01;
-            UInt32 actual;
-            bool conversionSucceded = Converter.ReadUIntBase128(stream, out actual);
-            Assert.IsTrue(conversionSucceded);
-            Assert.AreEqual(expected, actual);
-        }
     }
 }
